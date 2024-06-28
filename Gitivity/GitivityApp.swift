082cc196bbb621bcdcca870b04ee84cc00b658ce
darulfirmansyah <darulfirmansyah@gmail.com>
2024-06-28 11:19:57 +0700

    Initial Commit

---
+++ Gitivity/GitivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitivityApp.swift
+//  Gitivity
+//
+//  Created by Darul Firmansyah on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct GitivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
